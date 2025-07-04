c41166e5b18589d100395e132d9b5f26198eb66b
Louay Ghnima <117737215+louayh11@users.noreply.github.com>
2025-07-04 11:14:33 +0100

    fix  Sentry

--- api/app.js
+++ api/app.js
@@ -3,8 +3,16 @@ const cors = require("cors");
 const indexRouter = require("./routers/index");
 const bodyParser = require("body-parser");
 const config = require("./config");
+const Sentry = require("@sentry/node");
+
 
 const app = express();
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  tracesSampleRate: 1.0,
+});
+app.use(Sentry.Handlers.requestHandler());
+
 
 app.use(cors({ origin: ["http://academy.tabaani.co", "https://academy.tabaani.co", "academy.tabaani.co", "http://localhost:3000","https://host-academy-frontend-production.up.railway.app"],     credentials: true
 }));
@@ -13,17 +21,12 @@ app.use(express.json());
 
 // Routes
 app.use("/", indexRouter);
+app.use(Sentry.Handlers.errorHandler());
 
 app.listen(config.port, () =>
   console.log(`App is listening on URI http://localhost:${config.port}`)
 );
-const Sentry = require("@sentry/node");
 
-Sentry.init({
-  dsn: process.env.SENTRY_DSN,
-  tracesSampleRate: 1.0,
-});
 
-app.use(Sentry.Handlers.requestHandler());
+
 // ... tes routes
-app.use(Sentry.Handlers.errorHandler());
